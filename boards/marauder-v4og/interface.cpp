2e3038d1ec38f9439bac6a3d6d0c0dc884a52a9a
Pirata <bmorcelli@gmail.com>
2025-05-25 11:13:16 -0300

    Final touches

--- boards/marauder-v4og/interface.cpp
+++ boards/marauder-v4og/interface.cpp
@@ -7,25 +7,32 @@
 #include <soc/adc_channel.h>
 #include <CYD28_TouchscreenR.h>
 
-CYD28_TouchR touch(320,240);
+CYD28_TouchR touch(320, 240);
 
 /***************************************************************************************
 ** Function name: _setup_gpio()
 ** Location: main.cpp
 ** Description:   initial setup for the device
 ***************************************************************************************/
-void _setup_gpio() { }
+void _setup_gpio()
+{
+  pinMode(TFT_BL, OUTPUT);
+}
 
 /***************************************************************************************
 ** Function name: _post_setup_gpio()
 ** Location: main.cpp
 ** Description:   second stage gpio setup to make a few functions work
 ***************************************************************************************/
-void _post_setup_gpio() { 
-    if(!touch.begin(&SPI)) {
-            Serial.println("Touch IC not Started");
-            log_i("Touch IC not Started");
-        } else Serial.println("Touch IC Started");
+void _post_setup_gpio()
+{
+  if (!touch.begin(&SPI))
+  {
+    Serial.println("Touch IC not Started");
+    log_i("Touch IC not Started");
+  }
+  else
+    Serial.println("Touch IC Started");
 }
 
 /*********************************************************************
@@ -33,44 +40,60 @@ void _post_setup_gpio() {
 ** location: settings.cpp
 ** set brightness value
 **********************************************************************/
-void _setBrightness(uint8_t brightval) { }
+void _setBrightness(uint8_t brightval)
+{
+  if (brightval > 5)
+    digitalWrite(TFT_BL, HIGH);
+  else
+    digitalWrite(TFT_BL, LOW);
+}
 
 /*********************************************************************
 ** Function: InputHandler
 ** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
 **********************************************************************/
-void InputHandler(void) {
-  static long tm=millis();
-  if(millis()-tm>300 || LongPress) { // don´t allow multiple readings in less than 200ms
-    if(touch.touched()) { 
+void InputHandler(void)
+{
+  static long tm = millis();
+  if (millis() - tm > 300 || LongPress)
+  { // don´t allow multiple readings in less than 200ms
+    if (touch.touched())
+    {
       auto t = touch.getPointScaled();
       t = touch.getPointScaled();
-      tm=millis();
-        if(rotation==3) {
-            t.y = (tftHeight+20)-t.y;
-            t.x = tftWidth-t.x;
-        }
-        if(rotation==0) {
-            int tmp=t.x;
-            t.x = tftWidth-t.y;
-            t.y = tmp;
-        }
-        if(rotation==2) {
-            int tmp=t.x;
-            t.x = t.y;
-            t.y = (tftHeight+20)-tmp;
-        }
-        //Serial.printf("Touched at x=%d, y=%d, rot=%d\n", t.x, t.y, rotation);
+      tm = millis();
+      if (rotation == 3)
+      {
+        t.y = (tftHeight + 20) - t.y;
+        t.x = tftWidth - t.x;
+      }
+      if (rotation == 0)
+      {
+        int tmp = t.x;
+        t.x = tftWidth - t.y;
+        t.y = tmp;
+      }
+      if (rotation == 2)
+      {
+        int tmp = t.x;
+        t.x = t.y;
+        t.y = (tftHeight + 20) - tmp;
+      }
+      // Serial.printf("Touched at x=%d, y=%d, rot=%d\n", t.x, t.y, rotation);
 
-        if(!wakeUpScreen()) AnyKeyPress = true;
-        else return;
+      if (!wakeUpScreen())
+        AnyKeyPress = true;
+      else
+        return;
 
-        // Touch point global variable
-        touchPoint.x = t.x;
-        touchPoint.y = t.y;
-        touchPoint.pressed=true;
-        touchHeatMap(touchPoint);
-    } else touchPoint.pressed=false;
+      // Touch point global variable
+      touchPoint.x = t.x;
+      touchPoint.y = t.y;
+      touchPoint.pressed = true;
+      touchHeatMap(touchPoint);
+    }
+    else
+      touchPoint.pressed = false;
   }
 }
 
@@ -79,12 +102,11 @@ void InputHandler(void) {
 ** location: mykeyboard.cpp
 ** Turns off the device (or try to)
 **********************************************************************/
-void powerOff() { }
-
+void powerOff() {}
 
 /*********************************************************************
 ** Function: checkReboot
 ** location: mykeyboard.cpp
 ** Btn logic to tornoff the device (name is odd btw)
 **********************************************************************/
-void checkReboot() { }
+void checkReboot() {}
