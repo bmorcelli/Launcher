ee45be4cc24dd8b0a38eb3db545ec358fd46e978
Ming <zoumingrui96@qq.com>
2026-08-25 00:05:00 +0800

    test: use official ROM USB console routing

--- boards/m5stack-paper-mono/safe_bringup.cpp
+++ boards/m5stack-paper-mono/safe_bringup.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <esp_rom_uart.h>
 #include <esp_rom_caps.h>
+#include <esp_rom_sys.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
@@ -9,12 +10,14 @@
 #endif
 
 static void romUsbPuts(const char *text) {
-    while (*text != '\0') { esp_rom_output_putc(*text++); }
+    esp_rom_printf("%s", text);
 }
 
 void setup() {
     delay(1000);
+    esp_rom_output_switch_buffer(ESP_ROM_USB_SERIAL_DEVICE_NUM);
     esp_rom_output_set_as_console(ESP_ROM_USB_SERIAL_DEVICE_NUM);
+    esp_rom_install_channel_putc(2, nullptr);
     romUsbPuts("PAPERMONO ROM USB SAFE BRING-UP\r\n");
     romUsbPuts("NO PAPERMONO PERIPHERALS INITIALIZED\r\n");
 }
