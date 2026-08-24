17095cca89086a55cbbd9f577acb323baf641fb5
Ming <zoumingrui96@qq.com>
2026-08-24 21:35:34 +0800

    test: add PaperMono serial-only safe bring-up

---
+++ boards/m5stack-paper-mono/safe_bringup.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#ifndef PAPERMONO_SAFE_BRINGUP
+#error "safe_bringup.cpp must only be built with PAPERMONO_SAFE_BRINGUP enabled"
+#endif
+
+void setup() {
+    HWCDCSerial.begin(115200);
+
+    const uint32_t serialWaitStarted = millis();
+    while (!HWCDCSerial && (millis() - serialWaitStarted < 2000U)) { delay(10); }
+
+    HWCDCSerial.println();
+    HWCDCSerial.println("PAPERMONO SAFE BRING-UP STAGE 1");
+    HWCDCSerial.printf("Chip model: %s\r\n", ESP.getChipModel());
+    HWCDCSerial.printf("Chip revision: %u\r\n", ESP.getChipRevision());
+    HWCDCSerial.printf("CPU frequency: %lu MHz\r\n", static_cast<unsigned long>(ESP.getCpuFreqMHz()));
+    HWCDCSerial.printf("Flash size: %lu bytes\r\n", static_cast<unsigned long>(ESP.getFlashChipSize()));
+    HWCDCSerial.printf("PSRAM size: %lu bytes\r\n", static_cast<unsigned long>(ESP.getPsramSize()));
+    HWCDCSerial.printf("Free heap: %lu bytes\r\n", static_cast<unsigned long>(ESP.getFreeHeap()));
+    HWCDCSerial.printf("Reset reason: %d\r\n", static_cast<int>(esp_reset_reason()));
+    HWCDCSerial.println("NO M5Unified INIT");
+    HWCDCSerial.println("NO DISPLAY INIT");
+    HWCDCSerial.println("NO SD INIT");
+    HWCDCSerial.println("NO PMIC/IOE1 COMMANDS");
+    HWCDCSerial.flush();
+}
+
+void loop() {
+    for (;;) { ulTaskNotifyTake(pdTRUE, portMAX_DELAY); }
+}
